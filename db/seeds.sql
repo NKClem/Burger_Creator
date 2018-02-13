7e62a4e363f39d28ad22a7b02d6f7f0c1593276e
NKClem <nkuhn09@gmail.com>
2018-02-12 22:32:03 -0500

    initial site files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('mushroom and swiss burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('chickpea and kale burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('santa fe turkey burger', false);
