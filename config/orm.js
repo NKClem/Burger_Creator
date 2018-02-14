cdcb30e5f6cab6a2c225fb683722338266cf1b5f
NKClem <nkuhn09@gmail.com>
2018-02-14 09:38:09 -0500

    added burger_controller.js file in controllers directory

--- config/orm.js
+++ config/orm.js
@@ -8,9 +8,9 @@ const orm = {
             console.log(res);
         });
     },
-    insertOne: function(table, colTwo, colThree, newVal) {
-        let queryString = 'INSERT INTO ?? (??, ??) VALUES ?, 0';
-        connection.query(queryString, [table, colTwo, colThree, newVal], function(err, res) {
+    insertOne: function(table, cols, newVals) {
+        let queryString = 'INSERT INTO ?? (??) VALUES ?, 0';
+        connection.query(queryString, [table, cols, newVals], function(err, res) {
             if (err) throw err;
             console.log(res);
         });
