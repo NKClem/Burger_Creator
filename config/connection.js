7fd6ac30c6bc0bc15f71a2eca0fabd0661be741e
NKClem <nkuhn09@gmail.com>
2018-02-17 01:52:50 -0500

    added to connection

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
+        root: Key.port,
         host: 'localhost',
         user: 'root',
         password: Key.key,
