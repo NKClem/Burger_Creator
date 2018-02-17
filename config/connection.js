05d378ef276676e69277572a5ce3744a1bf2c846
NKClem <nkuhn09@gmail.com>
2018-02-17 02:30:33 -0500

    removed references to key.js file

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const mysql = require('mysql');
-const Key = require('./keys.js');
 
 let connection;
 
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const Key = require('./config/keys.js');
 const express = require('express');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
